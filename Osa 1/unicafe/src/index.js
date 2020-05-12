bfb791ff6e6624a83f6b16c4a239d9e79b027b01
Henri0088 <Henri.Sundquist@rokki.net>
2020-05-12 18:22:22 +0300

    step1

--- Osa 1/unicafe/src/index.js
+++ Osa 1/unicafe/src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Button = ({set, text}) => (
+  <button onClick={set}>
+    {text}
+  </button>
+)
+
+  
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <Button set={() => setGood(good + 1)} text='good' />
+        <Button set={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button set={() => setBad(bad + 1)} text='bad' />
+      </div>
+      <h1>Statistics</h1>
+      good {good} <br/>
+      neutral {neutral} <br/>
+      bad {bad} <br/>
+    </div>
+  )
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
