a75f6224d9837921b257928707b222a61a2826db
Henri <henri.sundquist@rokki.net>
2020-06-08 15:23:00 +0300

    4.5

--- Osa 4/blogilista/tests/list_helper.test.js
+++ Osa 4/blogilista/tests/list_helper.test.js
@@ -1,5 +1,32 @@
 const listHelper = require('../utils/list_helper')
 
+const blogList = [
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Test blog1',
+		author: 'Test person1',
+		url: 'example.com/yeet1',
+		likes: 5,
+		__v: 0
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Test blog2',
+		author: 'Test person2',
+		url: 'example.com/yeet2',
+		likes: 7,
+		__v: 0
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Test blog3',
+		author: 'Test person3',
+		url: 'example.com/yeet3',
+		likes: 2,
+		__v: 0
+	}
+]
+
 test('dummy returns one', () => {
 	const blogs = []
 
@@ -7,13 +34,13 @@ test('dummy returns one', () => {
 	expect(result).toBe(1)
 })
 
-describe('total likes', () => {
+describe('total likes: ', () => {
 	const oneBlog = [
 		{
 			_id: '5a422aa71b54a676234d17f8',
-			title: 'Test blog',
-			author: 'Test person',
-			url: 'example.com/yeet',
+			title: 'Test blog1',
+			author: 'Test person1',
+			url: 'example.com/yeet1',
 			likes: 5,
 			__v: 0
 		}
@@ -24,4 +51,24 @@ describe('total likes', () => {
 		expect(result).toBe(5)
 	})
 
+	test('empty list returns 0', () => {
+		expect(listHelper.totalLikes([])).toBe(0)
+	})
+
+	test('list of multiple blogs returns correct likes', () => {
+		const result = listHelper.totalLikes(blogList)
+		expect(result).toBe(14)
+	})
 })
+
+describe('favorite blog: ', () => {
+	test('returns correct blog correctly formatted from a hardcoded list', () => {
+		const fav = listHelper.favoriteBlog(blogList)
+		expect(fav).toEqual({
+			title: 'Test blog2',
+			author: 'Test person2',
+			likes: 7
+		})
+	})
+})
+
--- Osa 4/blogilista/utils/list_helper.js
+++ Osa 4/blogilista/utils/list_helper.js
@@ -3,12 +3,27 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
+	if (blogs.length === 0) {
+		return 0
+	}
+
 	const likeArray = blogs.map(blog => blog.likes)
 	const total = likeArray.reduce((sum, item) => sum + item, 0)
 	return total
 }
 
+const favoriteBlog = (blogs) => {
+	const likeArray = blogs.map(blog => blog.likes)
+	const indexOfMax = likeArray.indexOf(Math.max(...likeArray))
+	return {
+		title: blogs[indexOfMax].title,
+		author: blogs[indexOfMax].author,
+		likes: blogs[indexOfMax].likes
+	}
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
